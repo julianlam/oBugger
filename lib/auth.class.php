85d4926d4a3d70643fc49707da59ffb1d600c252
Julian Lam (Home) <julian.lam@gmail.com>
2011-07-22 19:32:12 +0000

    committing missing file

---
+++ lib/auth.class.php
@@ -0,0 +1,30 @@
+<?php
+	require_once 'common_run.inc.php';
+
+	class Auth {
+		public function isLoggedIn() {
+			if (isset($GLOBALS['obugger']['accountID']) && $GLOBALS['obugger']['accountID'] > 0) return 1;
+			else return 0;
+		}
+
+		public function login($username, $password) {
+			$db = db();
+			global $config;
+			$user = $db->run(
+				"SELECT * FROM users WHERE username=:username AND password=:password",
+				array(":username" => $_POST['username'], ":password" => md5($_POST['password'] . $config['security']['password_salt']))
+			)->fetch();
+			if ($user) {
+				$_SESSION['obugger'] = $user;
+				$GLOBALS['obugger'] = $user;
+				return 1;
+			}
+			else return 0;
+		}
+
+		public function logout() {
+			unset($GLOBALS['obugger']);
+			unset($_SESSION['obugger']);
+		}
+	}
+?>
