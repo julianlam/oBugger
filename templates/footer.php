c806c0a9996352db94a7f3ede6ec31320def8d6e
Julian Lam (Home) <julian.lam@gmail.com>
2011-08-26 15:06:07 +0000

    updating version number

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
 
 <div class="footer">
-	<a href="http://www.obugger.com">oBugger v0.25 alpha</a> - you use this bug tracker at your own risk!
+	<a href="http://www.obugger.com">oBugger v0.26 alpha</a> - you use this bug tracker at your own risk!
 </div>
